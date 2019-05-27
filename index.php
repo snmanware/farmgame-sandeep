6e62094b891a5c5f632993aa38b8cdf2f7f458ba
snmanware <snmanware@gmail.com>
2019-05-27 19:16:15 +0530

    adding logic

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <?php
 if($_SERVER['HTTP_HOST']) $DOMAIN = $_SERVER['HTTP_HOST']; else $DOMAIN = $_SERVER['SERVER_NAME'];
-
 $FOLDER = 'farmgame';
 define("SERVER_ABS_PATH",$_SERVER["DOCUMENT_ROOT"]."/".$FOLDER);
 define("SITE_URL",$DOMAIN."/".$FOLDER);
@@ -25,18 +20,47 @@ $str_table = $f->create_table();
 ?>
 
 <div class="container">
-<div class="row">
-<div>
-<table id="formtable" class="" border="1" width="600">
-<?php echo $str_table; ?>
-</table>
-</div><div><button id="starGame" >Click</button> <label>Your Clicks <span id="totalClicks">0</span></label><label>counter Clicks <span id="counterClicks">0</span></label></div>
-</div>
+      <div class="row">
+            <div>
+                  <table id="formtable" class="" border="1" width="500">
+                        <?php echo $str_table; ?>
+                  </table>
+            </div>
+            <div>
+            <h3>Game Statistics</h3>
+            
+                  <table width="50" border="1">
+                        <tr>
+                            <th>Your Clicks </th>
+                            <th>Faramer Dead</th>
+                            <th>Cows Dead</th>
+                            <th>Bunnies Dead</th>
+                        </tr>
+                        <tr>
+                            <td id="totalClicks">0</td>
+                            <td id="farmer_dead">0</td>
+                            <td id="cows_dead">0</td>
+                            <td id="bunnies_dead">0</td>
+                        </tr>
+                  </table>
+                  <button id="starGame" >Click</button>
+                  <label>Your Clicks 
+                        <span id="totalClicks">0</span>
+                  </label>
+                  <label>Game Sataus  
+                        <span id="Game_ends"></span>
+                  </label>
+                  
+            </div>
+      </div>
 </div>
 
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
   <script>
   var SITEURL = "<?php echo SITE_URL; ?>";
- 
   </script>
 
   <script src="js/common.js"></script>
--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 var GAMECLICK = 0;
+var stop_counter = 50;
 //var bunnies = [];
-
 var bunnies = {4:0, 5:0, 6:0,7:0};
 var total_animal_obj = {1:1,2:2,3:3,4:4,5:5,6:6,7:7};
 var cows ={2:0,3:0};
@@ -21,35 +21,22 @@ $.fn.actionOn = function(){
     $('#totalClicks').text(GAMECLICK);
     $('#counterClicks').text(counterClick);
     //console.log('totalClicks',GAMECLICK,'counterClick',counterClick,'animal_type',animal_type);
-    
     var data_num= actiontd.data('number');
     if(data_num>3) bunnies[data_num] = 1;
     if(data_num>1 && data_num<4) cows[data_num] = 1;
     if(data_num ==1) farmer[data_num] = 1;
     console.log('cows',cows);
     if(count_bunneis ==8){
-      //iterteOnAnimal(bunnies);
-        /*$.each(bunnies,function(i,v){ 
-            console.log(i,v,delet_number);
-            if(bunnies[i]==0){
-                $("td[data-number='"+i+"']").css('background-color','gray');
-                $("td[data-number='"+i+"']").addClass('dead');
-                delete bunnies[i];
-                delete total_animal_obj[i];
-                console.log('total_animal_obj',total_animal_obj,'bunnies',bunnies);
-            }else{
-                console.log('herer in else');
-                bunnies[i] =0;
-            } 
-        });*/
-        count_bunneis=0;
+      iterteOnAnimal(bunnies);
+      count_bunneis=0;
     }
     if(count_cows==10){
       //iterteOnAnimal(cows);
       count_cows =0;
     }
-    if(counterClick==15){
+    if(counterClick==10){
       iterteOnAnimal(farmer);
+
       counterClick =0;
     }
 
@@ -59,16 +46,19 @@ $.fn.actionOn = function(){
 function iterteOnAnimal(obj_animal){
       
   $.each(obj_animal,function(i,v){ 
-    console.log(i,v,delet_number);
+    console.log('count=>',i,v,delet_number);
     if(obj_animal[i]==0){
         $("td[data-number='"+i+"']").css('background-color','green');
         $("td[data-number='"+i+"']").addClass('dead');
         delete obj_animal[i];
         delete total_animal_obj[i];
-        console.log('total_animal_obj',total_animal_obj,'bunnies',obj_animal);
-        if(i==1){
-          console.log('stop the game');
-        }
+        changeScoreBoard(i);
+        console.log('total_animal_obj',total_animal_obj,'obj_animal',obj_animal);
+        //if(i==1){
+          //console.log('stop the game');
+          //$('#starGame').attr("disabled", true);
+          //$('#farmer_dead').text('1');
+       // }
     }else{
         console.log('herer in else');
         obj_animal[i] =0;
@@ -76,8 +66,28 @@ function iterteOnAnimal(obj_animal){
 });
     console.log('bunnies',bunnies,'cows',cows);
 }
+function changeScoreBoard(animal_number){
+  console.log('animal_number',animal_number);
+  if(animal_number==1){
+    $('#starGame').attr("disabled", true);
+
+          $('#farmer_dead').text('1');
+          $('#totalClicks').text(GAMECLICK);
+  }
+  if(animal_number>3) {
+    tb = $('#bunnies_dead').text();
+    console.log('----------------tb',tb);
+    tb=tb+1;
+    $('#farmer_dead').text('dead');
+    
+  }
+
+}
+
 $('document').ready(function(){
     $('#starGame').on('click',function(e){
+      $('#bunnies_dead').text('hell');
+      if(GAMECLICK<50){
         GAMECLICK++;
         counterClick++;
         count_bunneis++;
@@ -90,6 +100,11 @@ $('document').ready(function(){
         var random_num 	= total_animal_array[Math.floor(Math.random()*total_animal_array.length)];
         var randomtd = $('#tr'+GAMECLICK+' td').eq(random_num);
         $(randomtd).actionOn();
+      }else{
+        $('#Game_ends').text('Your Clicks are over!')
+        return false;
+      }
+        
     });
   
   });
