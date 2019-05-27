f4ca0aba5937a13690f9cebf19755829a1387142
snmanware <snmanware@gmail.com>
2019-05-27 19:19:10 +0530

    adding logic

--- js/common.js
+++ js/common.js
@@ -15,8 +15,7 @@ var ANIMAL_STATUS = 0;
 
 $('document').ready(function(){
   $('#starGame').on('click',function(e){
-    ///$('#bunnies_dead').text('hell');
-      if(GAMECLICK<50){
+      if(GAMECLICK<stop_counter){
           GAMECLICK++;
           FARMERCLICKS++;
           BUNNYCLICK++;
@@ -42,7 +41,6 @@ $.fn.actionOn = function(){
     if(data_num>3) bunnies[data_num] = 1;
     if(data_num>1 && data_num<4) cows[data_num] = 1;
     if(data_num ==1) farmer[data_num] = 1;
-    //console.log('cows',cows);
     if(BUNNYCLICK ==8){
         checkWhoIsDead(bunnies);
         BUNNYCLICK=0;
@@ -51,47 +49,46 @@ $.fn.actionOn = function(){
         checkWhoIsDead(cows);
         COWSCLICK =0;
     }
-    if(FARMERCLICKS==10){
+    if(FARMERCLICKS==15){
         checkWhoIsDead(farmer);
         FARMERCLICKS =0;
     }
-   // console.log('bunnies',bunnies,'total_animal_obj',total_animal_obj);
     actiontd.css('background-color','green').html('fed');
 }
 function checkWhoisRemained(){
   //console.log('total_animal_obj',total_animal_obj);
-  var lb = Object.keys(bunnies).length;
-  var lc = Object.keys(cows).length;
+  var lb = Object.keys(bunnies).length; // count remained bunnies
+  var lc = Object.keys(cows).length;// count remained cows
     
   if(lb >0  && lc>0){
-      return 'Conratulations! You are the Winner !';
+      msg = 'Conratulations! You are the Winner !';
     }else{
-      return 'You loss the game! To win you need a cow and a bunnie alive';
+      var msg ='You loss the game! To win you need a cow and a bunnie alive';
+      
     }
+    alert(msg);
 }
 function checkWhoIsDead(obj_animal){
     $.each(obj_animal,function(i,v){ 
-    //console.log('count=>',i,v,delet_number);
         if(obj_animal[i]==0){
             $("td[data-number='"+i+"']").css('background-color','green');
             $("td[data-number='"+i+"']").addClass('dead');
             delete obj_animal[i];
             delete total_animal_obj[i];
             changeScoreBoard(i);
-            //console.log('total_animal_obj',total_animal_obj,'obj_animal',obj_animal);
         }else{
-            //console.log('herer in else');
             obj_animal[i] =0;
         } 
     });
-    //console.log('bunnies',bunnies,'cows',cows);
 }
 function changeScoreBoard(animal_number){
-  //console.log('animal_number',animal_number);
   $('#th'+animal_number).css('background-color','red');
     if(animal_number==1){
         $('.animal-all').css('background-color','red');
-        $('#game_status strong').text('Game is Over! You loss the Game! , Please try you luck again.');
+        var msg = 'Game is Over! You loss the Game! , Please try you luck again.';
+        $('#game_status strong').text(msg);
+        alert(msg);
+
         $('#starGame').attr("disabled", true);
             $('#farmer_dead').text('1').css('background-color','red');
             $('#totalClicks').text(GAMECLICK);
